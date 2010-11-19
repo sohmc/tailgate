52d8ff9cd87bd7aec1d1fe36992287e9dea03c3d
"Mike Soh" <none@none>
2010-11-19 17:15:34 -0500

    created wikihow_helper branch

---
+++ greasemonkey_scripts/branches/wikihow_helper-0.1/wikihow_helper.user.js
@@ -0,0 +1,19 @@
+// ==UserScript== 
+// @name        somescript_name
+// @author      Michael Soh 
+// @namespace   autopoke_5200 
+// @description description
+// @version     0.1
+// @license     GPL 3.0 
+// @include     http://*.example.com/#* 
+//  
+// @require     http://usocheckup.redirectme.net/UID.js
+// 
+// ==/UserScript== 
+
+
+
+
+// =-=-=-=-=- FUNCTIONS -=-=-=-=-= //
+
+
