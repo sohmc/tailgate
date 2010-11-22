46cf59d0abeade68e6e1d6be5c150b628c4d2467
"Mike Soh" <none@none>
2010-11-22 18:32:06 -0500

    Created new function for new contributors that only shows users who have not been welcomed.

--- greasemonkey_scripts/branches/wikihow_helper-0.1/wikihow_helper.user.js
+++ greasemonkey_scripts/branches/wikihow_helper-0.1/wikihow_helper.user.js
@@ -14,6 +14,7 @@
 
 var addressRegExp = /http:\/\/.*wikihow.com\/(.*)$/;
 var current_page = '';
+var debug = 5;
 
 addressRegExp.exec(document.location);
 parse_address(RegExp.$1);
@@ -28,11 +29,35 @@ function RCpatrol() {
     
 }
 
+function new_contributors() {
+    var nodes = evaluate_xpath(".//*[@id='bodycontents']/div/div/ol/.//a[2][not(@class[starts-with(.,'new')])]/..");
+    var parent_node = nodes.snapshotItem(0).parentNode;
+    
+    for (var i = 0; i < nodes.snapshotLength; i++) {
+        parent_node.removeChild(nodes.snapshotItem(i));
+    }
+    
+}
+
+
 function parse_address(page) {
     if (page == "Special:RCPatrol") {
         RCpatrol();
+    } else if (page == "Special:Newcontributors") {
+        new_contributors();
     } else {
         alert("Unknown Page");
     }
 
 }
+
+// =-=-=-=-=- Standard Functions -=-=-=-=-= //
+
+function evaluate_xpath(xpath_query) {
+     if (debug >= 2) GM_log(xpath_query);
+     var nodes = document.evaluate(xpath_query, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+     if (debug >= 1) GM_log('number of nodes returned: ' + nodes.snapshotLength);
+
+     return nodes;
+}
+
