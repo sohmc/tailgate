ee90b9c9cdbe30568f1275885c8c145d2bd23bd1
"Mike Soh" <none@none>
2010-11-26 15:25:48 -0500

    started div

--- greasemonkey_scripts/branches/wikihow_helper-0.1/wikihow_helper.user.js
+++ greasemonkey_scripts/branches/wikihow_helper-0.1/wikihow_helper.user.js
@@ -16,11 +16,9 @@ var addressRegExp = /http:\/\/.*wikihow.com\/(.*)$/;
 var current_page = '';
 var debug = 5;
 
+
 addressRegExp.exec(document.location);
 parse_address(RegExp.$1);
-
-
-
 
 
 // =-=-=-=-=- FUNCTIONS -=-=-=-=-= //
@@ -50,6 +48,7 @@ function leave_message() {
 function new_contributors() {
     var nodes = evaluate_xpath(".//*[@id='bodycontents']/div/div/ol/.//a[2][not(@class[starts-with(.,'new')])]/..");
     show_only_new_users(nodes);
+    insert_comment_div();
 }
 
 function create_quick_edits() {
@@ -121,6 +120,20 @@ function initQuickNote( qnArticle, qnUser, contrib, regdate ) {
 
 function insert_comment_div() {
     var comment_div = document.createElement('div');
+    comment_div.setAttribute('id', 'modalContainer');
+    comment_div.setAttribute('class', 'modalContainer');
+    comment_div.innerHTML = " \
+        <link href="http://pad3.whstatic.com/extensions/min/?f=extensions/wikihow/popupEdit.css,skins/WikiHow/articledialog.css&rev=2110" rel="stylesheet">
+        <img height=\"10\" width=\"679\" style=\"display: block;\" src=\"http://pad2.whstatic.com/skins/WikiHow/images/article_top.png\"> \
+        <div class=\"modalContent\" id=\"quicknotecontent\"> \
+        <div id=\"modalHeader\"> \
+                <a id=\"modal_x\" onclick=\"document.getElementById('modalPage').style.display = 'none';\"></a> \
+                <img alt=\"wikiHow\" id=\"modal_logo\" src=\"http://pad1.whstatic.com/skins/WikiHow/images/wikihow.gif\"> \
+        </div><!--end editModalHeader--> \
+        <div class=\"modalBody\"> \
+        </div> \
+</div>";
+    document.body.insertBefore(comment_div, document.body.firstChild);
 
 }
 
