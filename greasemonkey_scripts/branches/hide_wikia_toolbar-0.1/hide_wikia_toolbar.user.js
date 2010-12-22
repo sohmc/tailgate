675cbd4045c9e3cbf10f0043cd80e2da8daa1fbb
"Mike Soh" <none@none>
2010-12-21 20:06:01 -0500

    New script: hide_wikia_toolbar

---
+++ greasemonkey_scripts/branches/hide_wikia_toolbar-0.1/hide_wikia_toolbar.user.js
@@ -0,0 +1,19 @@
+// ==UserScript== 
+// @name        Hide Wikia Toolbar
+// @author      Michael Soh 
+// @namespace   hide_wikia_toolbar_xxx
+// @description description
+// @version     0.1
+// @license     GPL 3.0 
+// @include     http://*.wikia.com/*
+//  
+// 
+// ==/UserScript== 
+
+
+// @require     http://usocheckup.redirectme.net/UID.js
+
+
+// =-=-=-=-=- FUNCTIONS -=-=-=-=-= //
+
+
