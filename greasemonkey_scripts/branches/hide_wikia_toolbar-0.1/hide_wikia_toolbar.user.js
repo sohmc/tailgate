0a72c296590e580b0da69bbcbfe43eb9473ecc63
"Mike Soh" <none@none>
2010-12-21 20:11:05 -0500

    It works out of the box.

--- greasemonkey_scripts/branches/hide_wikia_toolbar-0.1/hide_wikia_toolbar.user.js
+++ greasemonkey_scripts/branches/hide_wikia_toolbar-0.1/hide_wikia_toolbar.user.js
@@ -10,10 +10,21 @@
 // 
 // ==/UserScript== 
 
+var nodes = evaluate_xpath(".//*[@id='WikiaFooter']/div");
+if (nodes.snapshotLength > 0) {
+    nodes.snapshotItem(0).setAttribute("style", "display: none");
+}
 
 // @require     http://usocheckup.redirectme.net/UID.js
 
 
 // =-=-=-=-=- FUNCTIONS -=-=-=-=-= //
 
+function evaluate_xpath(xpath_query) { 
+     if (debug >= 2) GM_log(xpath_query); 
+     var nodes = document.evaluate(xpath_query, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null); 
+     if (debug >= 1) GM_log('nodes returned: ' + nodes.snapshotLength); 
+ 
+     return nodes; 
+} 
 
