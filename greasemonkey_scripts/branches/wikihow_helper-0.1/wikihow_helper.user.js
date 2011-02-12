0c6f6fc643dfdd508d1531aa040287131a4f20e4
"Mike Soh" <none@none>
2011-02-11 20:11:41 -0500

    track which user is clicked

--- greasemonkey_scripts/branches/wikihow_helper-0.1/wikihow_helper.user.js
+++ greasemonkey_scripts/branches/wikihow_helper-0.1/wikihow_helper.user.js
@@ -53,18 +53,19 @@ function new_contributors() {
     for (var i = 0; i < nodes.snapshotLength; i++) {
         var this_node = nodes.snapshotItem(i);
         var talk_link = this_node.getAttribute("title");
-        var reg = /User_talk:(\S+)/;
+        var reg = /User_talk:(.+)/;
         reg.exec(talk_link);
         this_node.setAttribute("title", "Click to send a message to " + RegExp.$1);
-        this_node.addEventListener("click", create_quick_edits('foobar'), true);
+        this_node.addEventListener("click", create_quick_edits, true);
         this_node.setAttribute("href", "#");
+        this_node.setAttribute("user", talk_link);
     }
 
 //    insert_comment_div();
 }
 
-function create_quick_edits(text) {
-    alert(text);
+function create_quick_edits() {
+    alert(this.getAttribute("user"));
 }
 
 function show_only_new_users(nodes_to_remove) {
