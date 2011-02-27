e8e37d424c5b91ff0ddd29e646bf5c46c0b7ccf0
"Mike Soh" <none@none>
2011-02-27 09:44:52 -0500

    seems to be working but not properly hiding.

--- greasemonkey_scripts/branches/zagg-0.1/zagg.user.js
+++ greasemonkey_scripts/branches/zagg-0.1/zagg.user.js
@@ -15,26 +15,31 @@ var debug = 5;
 
 var replace_links = evaluate_xpath(document, ".//a[@href[contains(.,'view_order.php?id=')]]/..[a='Replace']/a");
 
-//for (var i = 0; i < replace_links.snapshotLength; i++) {
-for (var i = 0; i < 1; i++) {
+for (var i = 0; i < replace_links.snapshotLength; i++) {
+//for (var i = 0; i < 1; i++) {
      GM_log('inspecting item: ' + i);
      var p = replace_links.snapshotItem(i);
      GM_log('href: ' + p.getAttribute('href'));
 
-     var req = new XMLHttpRequest();
-     req.overrideMimeType('text/xml');
-     req.open("GET", "https://www.zagg.com/support/account/" + p.getAttribute('href'), false);
-     req.send(null);
+     GM_xmlhttpRequest({
+          method:'GET',
+          url:p.getAttribute('href'),
+          onload: function(r) {
+               GM_log('connection status: ' + r.status);
+               if (r.status == 200) {
+                    GM_log('site returned ' + r.responseText.length + ' characters.');
+                    if (r.responseText.indexOf('alt="Unavailible" title="Replacement Availability"') != -1) {
+                         GM_log(i + ': Unavailable');
+                         p.style.display = 'none';
+                    } else {
+                         GM_log(i + ': AVAILABLE');
+                         p.style.color = 'green';
+                    }
+               }
 
-     var xmlDoc = req.responseXML;
-     var nsResolver = xmlDoc.createNSResolver( xmlDoc.ownerDocument == null ? xmlDoc.documentElement : xmlDoc.ownerDocument.documentElement);
-
-     var img_nodes = xmlDoc.evaluate('//*', xmlDoc, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-     for (var i = 0; i < img_nodes.snapshotLength; i++) {
-          GM_log(i + ": " + img_nodes.snapshotItem(i).innerHTML);
-     }
+          }
+     });
 }
-
 
 // =-=-=-=-=- FUNCTIONS -=-=-=-=-= //
 
