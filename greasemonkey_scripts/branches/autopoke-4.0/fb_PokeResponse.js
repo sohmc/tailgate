8e06455b62674a08910978ae605c0639834ec425
sohmc <none@none>
2011-03-25 16:21:50 -0400

    added some additional lines for comparison

--- greasemonkey_scripts/branches/autopoke-4.0/fb_PokeResponse.js
+++ greasemonkey_scripts/branches/autopoke-4.0/fb_PokeResponse.js
@@ -1,3 +1,35 @@
+// =-=-=-=-=- ENGLISH -=-=-=-=-= 
 //Haven't poked yet:
-
 for (;;);{"__ar":1,"payload":null,"onload":["new Dialog({\"secure\":true,\"title\":{\"__html\":\"Poke Bob?\"},\"body\":{\"__html\":\"\\u003cdiv class=\\\"UIImageBlock clearfix\\\">\\u003cimg class=\\\"UIImageBlock_Image UIImageBlock_MED_Image img\\\" src=\\\"https:\\\/\\\/fbcdn-profile-a.akamaihd.net\\\/hprofile-ak-snc4\\\/111111111_s.jpg\\\" alt=\\\"\\\" \\\/>\\u003cdiv class=\\\"UIImageBlock_Content UIImageBlock_MED_Content\\\">\\u003cinput type=\\\"hidden\\\" id=\\\"post_form_id\\\" name=\\\"post_form_id\\\" value=\\\"abcdefghijklmnopqrstuvwxyz0123456789\\\" autocomplete=\\\"off\\\" \\\/>You are about to poke Bob. He will be informed of this on his home page.\\u003cinput type=\\\"hidden\\\" autocomplete=\\\"off\\\" name=\\\"uid\\\" value=\\\"1234567890\\\" \\\/>\\u003cinput type=\\\"hidden\\\" autocomplete=\\\"off\\\" name=\\\"pokeback\\\" value=\\\"1\\\" \\\/>\\u003cinput type=\\\"hidden\\\" autocomplete=\\\"off\\\" name=\\\"opp\\\" value=\\\"\\\" \\\/>\\u003c\\\/div>\\u003c\\\/div>\"},\"buttons\":[{\"name\":\"pk01\",\"label\":\"Poke\"},\"cancel\"],\"postURI\":[\"\\\/ajax\\\/poke.php\",true]}).show();"],"dontReplay":true}
+
+//HTML
+
+
+
+//Poke Results:
+for (;;);{"__ar":1,"payload":{"secure":true,"title":{"__html":"Poked Bob Johnson"},"body":{"__html":"\u003cdiv class=\"UIImageBlock clearfix\">\u003cimg class=\"UIImageBlock_Image UIImageBlock_MED_Image img\" src=\"https:\/\/fbcdn-profile-a.akamaihd.net\/hprofile-ak-snc4\/111111111.jpg\" alt=\"\" \/>\u003cdiv class=\"UIImageBlock_Content UIImageBlock_MED_Content\">You have poked Bob Johnson.\u003c\/div>\u003c\/div>"},"buttons":["ok"],"handler":"","autohide":1100},"onload":["setCookie(\"made_write_conn\", \"1234567890\", 20000);","setCookie(\"W\", \"1234567890\", 20000);"],"invalidate_cache":[0]}
+
+
+//Already poked:
+for (;;);{"__ar":1,"payload":{"secure":true,"title":{"__html":"Error"},"body":{"__html":"\u003cdiv class=\"UIImageBlock clearfix\">\u003cimg class=\"UIImageBlock_Image UIImageBlock_MED_Image img\" src=\"https:\/\/fbcdn-profile-a.akamaihd.net\/hprofile-ak-snc4\/111111111.jpg\" alt=\"\" \/>\u003cdiv class=\"UIImageBlock_Content UIImageBlock_MED_Content\">Bob has not received your last poke yet. He&#039;ll get it the next time he logs in.\u003c\/div>\u003c\/div>"},"buttons":["ok"]}}
+
+
+
+// =-=-=-=-=- Korean -=-=-=-=-=
+// Haven't poked yet:
+for (;;);{"__ar":1,"payload":{"secure":true,"title":{"__html":"Alice\ub2d8\uc744 \ucf55 \ucc14\ub7ec\ubcf4\uc2dc\ub824\uad6c\uc694?"},"body":{"__html":"\u003cdiv class=\"UIImageBlock clearfix\">\u003cimg class=\"UIImageBlock_Image UIImageBlock_MED_Image img\" src=\"https:\/\/fbcdn-profile-a.akamaihd.net\/hprofile-ak-snc4\/1234567890.jpg\" alt=\"\" \/>\u003cdiv class=\"UIImageBlock_Content UIImageBlock_MED_Content\">\u003cinput type=\"hidden\" id=\"post_form_id\" name=\"post_form_id\" value=\"abcd1234\" autocomplete=\"off\" \/>Alice\ub2d8\uc744 \ucf55 \ucc0c\ub974\ub824\uace0 \ud569\ub2c8\ub2e4. \ud648\ud398\uc774\uc9c0\uc5d0\uc11c \ud655\uc778\ud560 \uc218 \uc788\uc2b5\ub2c8\ub2e4.\u003cinput type=\"hidden\" autocomplete=\"off\" name=\"uid\" value=\"1234567890\" \/>\u003cinput type=\"hidden\" autocomplete=\"off\" name=\"pokeback\" value=\"0\" \/>\u003cinput type=\"hidden\" autocomplete=\"off\" name=\"opp\" \/>\u003c\/div>\u003c\/div>"},"buttons":[{"name":"pk01","label":"\ucf55 \ucc14\ub7ec\ubcf4\uae30"},"cancel"],"postURI":["\/ajax\/poke.php",true]}}
+
+//HTML
+//<div class="UIImageBlock clearfix"><img class="UIImageBlock_Image UIImageBlock_MED_Image img" src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc4/1234567890.jpg" alt="" /><div class="UIImageBlock_Content UIImageBlock_MED_Content"><input type="hidden" id="post_form_id" name="post_form_id" value="abcd1234" autocomplete="off" />Alice?? ? ???? ???. ?????? ??? ? ????.<input type="hidden" autocomplete="off" name="uid" value="1234567890" /><input type="hidden" autocomplete="off" name="pokeback" value="1" /><input type="hidden" autocomplete="off" name="opp" value="" /></div></div>
+
+
+//Poke Results:
+for (;;);{"__ar":1,"payload":{"secure":true,"title":{"__html":"Alice Johnson\ub2d8\uc744 \ucf55 \ucc14\ub800\uc2b5\ub2c8\ub2e4."},"body":{"__html":"\u003cdiv class=\"UIImageBlock clearfix\">\u003cimg class=\"UIImageBlock_Image UIImageBlock_MED_Image img\" src=\"https:\/\/fbcdn-profile-a.akamaihd.net\/hprofile-ak-snc4\/1234567890.jpg\" alt=\"\" \/>\u003cdiv class=\"UIImageBlock_Content UIImageBlock_MED_Content\">Alice Johnson\ub2d8\uc744 \ucf55 \ucc14\ub800\uc2b5\ub2c8\ub2e4.\u003c\/div>\u003c\/div>"},"buttons":["ok"],"handler":"","autohide":1100},"onload":["setCookie(\"made_write_conn\", \"1234567890\", 20000);","setCookie(\"W\", \"1234567890\", 20000);"],"invalidate_cache":[0]}
+
+
+//Already poked:
+for (;;);{"__ar":1,"payload":{"secure":true,"title":{"__html":"\uc624\ub958"},"body":{"__html":"\u003cdiv class=\"UIImageBlock clearfix\">\u003cimg class=\"UIImageBlock_Image UIImageBlock_MED_Image img\" src=\"https:\/\/fbcdn-profile-a.akamaihd.net\/hprofile-ak-snc4\/12345697890.jpg\" alt=\"\" \/>\u003cdiv class=\"UIImageBlock_Content UIImageBlock_MED_Content\">Alice\ub2d8\uaed8 \ubcf4\ub0b8 \ucf55 \ucc14\ub7ec\ubcf4\uae30\ub294 Alice\ub2d8\uc774 \ub2e4\uc74c\uc5d0 \ub85c\uadf8\uc778\ud558\uba74 \ubc1b\uac8c \ub429\ub2c8\ub2e4.\u003c\/div>\u003c\/div>"},"buttons":["ok"]}}
+
+
+
+
