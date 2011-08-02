55dfa3bed9839ef20a09aa3872539e0ab4f70dab
sohmc <none@none>
2011-08-02 10:11:45 -0400

    Added new script: interfacelift downloader

---
+++ greasemonkey_scripts/branches/interfacelift-downloader-0.1/interfacelift-downloader.user.js
@@ -0,0 +1,19 @@
+// ==UserScript== 
+// @name        Interfacelift Downloader
+// @author      Michael Soh 
+// @namespace   interfacelift-- 
+// @description Allows for bulk downloading of desktop images
+// @version     0.1
+// @license     GPL 3.0 
+// @include     http://interfacelift.com/*
+//  
+// 
+// ==/UserScript== 
+// @require     http://usocheckup.redirectme.net/UID.js
+
+
+
+
+// =-=-=-=-=- FUNCTIONS -=-=-=-=-= //
+
+
