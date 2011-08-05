2b35e20c84e0e4af81f5660ccaf7036ec7504d5d
sohmc <none@none>
2011-08-05 12:33:03 -0400

    modified extension id.

--- firefox_addons/branches/interfaces_downloader-0.1/defaults/preferences/prefs.js
+++ firefox_addons/branches/interfaces_downloader-0.1/defaults/preferences/prefs.js
@@ -3,4 +3,4 @@ pref("extensions.interfacesdownloader.intpref", 0);
 pref("extensions.interfacesdownloader.stringpref", "A string");
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.5ac32dab6639ff25e31eb2772100ac67.description", "chrome://interfacesdownloader/locale/overlay.properties");
+pref("extensions.interfacesdownloader@tailgate.googlecode.com.description", "chrome://interfacesdownloader/locale/overlay.properties");
