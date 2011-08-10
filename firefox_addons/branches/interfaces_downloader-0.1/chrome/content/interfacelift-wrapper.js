00e2121b0d77c728a14cf8c99f47192850248305
sohmc <none@none>
2011-08-10 11:06:23 -0400

    jQuery now works!

--- firefox_addons/branches/interfaces_downloader-0.1/chrome/content/interfacelift-wrapper.js
+++ firefox_addons/branches/interfaces_downloader-0.1/chrome/content/interfacelift-wrapper.js
@@ -1,10 +1,62 @@
-dump("sourced interfacelift wrapper.\n");
+dump("sourcing interfacelift wrapper...");
 var interfaceliftdownloader = function(doc) {
      this.hostname = location.hostname;
      this.debug = 3;
      this.document = doc;
 
-     this.at_interfacelift = function(doc) {
+     this.at_interfacelift = function () {
+          this.build_gui();
+          this.initialize_interface();
+     };
+
+     this.initialize_interface = function() {
+          var jQuery = interfacesdownloader.jQuery;
+          var $ = function(selector, context) {
+               return new jQuery.fn.init(selector, context || window._content.document);
+          };
+          $.fn = $.prototype = jQuery.fn;
+          interfacesdownloader.env = window._content.document;
+
+          $("body").click(function() { alert('jQuery Works'); });
+
+          var download_a = this.xpath(".//div[@id[starts-with(.,'download')]]/a");
+          var download_div = this.xpath(".//div[@id[starts-with(.,'download')]]/a/..");
+          var download_preview = this.xpath(".//div[@id[starts-with(.,'download')]]/a/../../../a/img");
+
+          for (var i = 0; i < download_div.snapshotLength; i++) {
+               if (this.debug >= 3) dump("modify " + i + "\n");
+               var href = "http://" + this.hostname + download_a.snapshotItem(i).getAttribute('href');
+               var re = /^download_(\d+)/.exec(download_div.snapshotItem(i).getAttribute('id'));
+               var id = re[1];
+
+               var preview = download_preview.snapshotItem(i).getAttribute('src');
+
+               if (doc.getElementById('op_ifdl_' + id)) {
+                    download_div.snapshotItem(i).innerHTML = '<input type="checkbox" name="dl" preview="' + preview + '" value="' + href + '" id="ifdl_' + id + '"checked="true" />';
+               } else {
+                    download_div.snapshotItem(i).innerHTML = '<input type="checkbox" name="dl" preview="' + preview + '" value="' + href + '" id="ifdl_' + id + '"/>';
+               }
+
+          }
+     };
+
+     this.toggle_download = function (t) {
+     };
+
+     this.build_gui = function () {
+          var ads = this.xpath('.//div[@id="sidebar"]/div[@class="ad"]');
+          var sidebar_parent = this.xpath('.//div[@id="sidebar"]');
+          
+          for (var i = 0; i < ads.snapshotLength; i++) {
+               sidebar_parent.snapshotItem(0).removeChild(ads.snapshotItem(i));
+               if (this.debug >= 3) dump("removed " + i + "\n");
+          }
+
+          var ifdl_gui = doc.createElement('div');
+          ifdl_gui.setAttribute('id', 'interface_dl_div');
+          ifdl_gui.setAttribute('style', 'width: 180px; height: 400px; position: fixed; z-index: 100; text-align: center; top: 10px;');
+          ifdl_gui.innerHTML = "<div id='preview_box' class='preview' style='width: 180px; height: 112px;'></div><select name='images' id='images' size='10' multiple='true' style='width: 175px;'></select><br><input type='button' value='Download Wallpaper' id='download_wallpaper'><span id='img_count' style='padding-left: 10px;'>0</span>";
+          sidebar_parent.snapshotItem(0).appendChild(ifdl_gui);
 
      };
 
@@ -17,3 +69,4 @@ var interfaceliftdownloader = function(doc) {
      };
 
 }
+dump("Done.\n");
--- firefox_addons/branches/interfaces_downloader-0.1/chrome/content/overlay.js
+++ firefox_addons/branches/interfaces_downloader-0.1/chrome/content/overlay.js
@@ -4,6 +4,7 @@ var interfacesdownloader = {
     this.initialized = true;
     this.strings = document.getElementById("interfacesdownloader-strings");
     var appcontent = document.getElementById("appcontent");
+
     if (appcontent) 
          appcontent.addEventListener("DOMContentLoaded", interfacesdownloader.onPageLoad, true);
   },
@@ -12,16 +13,28 @@ var interfacesdownloader = {
      var doc = aEvent.originalTarget;
      var f = new interfaceliftdownloader(doc);
 
-     f.xpath('.//*');
-
      if (doc.nodeName == "#document") {
           var at_interfaces = /http:\/\/.*interfacelift\.com\/.*/.test(doc.location.href);
 
           if (at_interfaces) {
+               interfacesdownloader.loadjQuery(interfacesdownloader);
                f.at_interfacelift(aEvent.target);
           }
      }
 
+  },
+
+  loadjQuery: function(context){
+     dump("Loading jQuery...");
+     var loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+                 .getService(Components.interfaces.mozIJSSubScriptLoader);
+     loader.loadSubScript("chrome://interfacesdownloader/content/jquery-1.6.2.js",context);
+
+     var jQuery = window.jQuery.noConflict(true);
+     if( typeof(jQuery.fn._init) == 'undefined') { jQuery.fn._init = jQuery.fn.init; }
+     interfacesdownloader.jQuery = jQuery;
+
+     dump("Done.\n");
   },
 
   onMenuItemCommand: function(e) {
