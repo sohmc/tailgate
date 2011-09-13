8fb287d13dcba19586762eed36baeeffe5f447b9
sohmc <none@none>
2011-09-13 15:45:36 -0400

    Creating new greasemonkey script to combat evil RSS scripts

---
+++ greasemonkey_scripts/branches/rss_expander-0.1/rss_expander.user.js
@@ -0,0 +1,20 @@
+// ==UserScript== 
+// @name        RSS Expander
+// @author      Michael Soh 
+// @namespace   rss_expander_5200 
+// @description description
+// @version     0.1
+// @license     GPL 3.0 
+// @include     http://www.google.com/reader/* 
+// @include     https://www.google.com/reader/* 
+//  
+// 
+// ==/UserScript== 
+// @require     http://usocheckup.redirectme.net/UID.js
+
+
+
+
+// =-=-=-=-=- FUNCTIONS -=-=-=-=-= //
+
+
