5c697d93ff35eca94341a856626653a8bfb43ac0
sohmc <none@none>
2011-09-14 09:42:45 -0400

    finally got it to retrieve the link.

--- greasemonkey_scripts/branches/rss_expander-0.1/rss_expander.user.js
+++ greasemonkey_scripts/branches/rss_expander-0.1/rss_expander.user.js
@@ -12,15 +12,52 @@
 // ==/UserScript== 
 // @require     http://usocheckup.redirectme.net/UID.js
 
-$(document).ready(function() {
-     $('#entries').scroll(function() {
-          GM_log('Scrolling!');
-     });
+debug = 3;
 
+$(document).ready(function() {
+     $('#entries').scroll(function() { process_links(); });
+     $('div[class^="entry"]').click(function() { process_links(); });
 });
 
 
 
 // =-=-=-=-=- FUNCTIONS -=-=-=-=-= //
 
+function process_links() {
+     GM_log('processing...');
+     var cards = xpath('.//div[@class="card-content"]');
+
+     for (var i = 0; i < 2; i++) {
+          var c = cards.snapshotItem(i);
+          GM_log("xml: " + c.innerHTML);
+          var xml = string_to_xml(c.innerHTML);
+
+          var link = xpath('.//h2/a[@class="entry-title-link"]', xml);
+          if (link) {
+               var l = link.snapshotItem(0);
+               GM_log("i=" + i + " --  node name: " + l.nodeName + " node type: " + l.nodeType + "\n" + l.textContent);
+               GM_log("href: " + l.getAttribute('href'));
+          }
+     }
+
+
+}
+
+
+function string_to_xml(s) {
+     var parser = new DOMParser();
+     var dom = parser.parseFromString(s, 'text/xml');
+
+     return dom;
+}
+
+function xpath(xpath_query, xml) { 
+     if (!xml) xml = document;
+
+     if (debug >= 2) GM_log(xpath_query); 
+     var nodes = xml.evaluate(xpath_query, xml, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null); 
+     if (debug >= 2) GM_log('nodes returned: ' + nodes.snapshotLength); 
+ 
+     return nodes; 
+}
 
