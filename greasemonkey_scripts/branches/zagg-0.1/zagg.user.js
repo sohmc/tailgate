f707e90c185f01e84b8894fc08d807114f4843cf
Mike Soh <none@none>
2012-03-22 12:55:15 -0400

    Updating script to accept more than two tables.

--- greasemonkey_scripts/branches/zagg-0.1/zagg.user.js
+++ greasemonkey_scripts/branches/zagg-0.1/zagg.user.js
@@ -13,52 +13,111 @@
 // @require     http://usocheckup.redirectme.net/98173.js
 
 
-var debug = 0;
+var debug = 5;
 
 // remove all warranty replacements
-var replace_links = evaluate_xpath(".//h3[contains(.,'Warranty Replacement Order History')]/..//table[1]");
-replace_links.snapshotItem(0).style.display = 'none';
+var tables = xpath_snapshot(".//table");
 
-replace_links = evaluate_xpath(".//h3[contains(.,'Warranty Replacement Order History')]/..//table[1]/.//a[contains(.,'Replace')]");
+for (var t = 0; t < tables.snapshotLength; t++) {
+     var table_rows = xpath_snapshot(".//table[" + (t+1) + "]/.//tr[not(contains(.,'Invoice Number'))]");
+
+     for (var i = 0; i < table_rows.snapshotLength; i++) {
+          row = table_rows.snapshotItem(i);
+
+          var r = xpath_snapshot(".//table[" + (t+1) + "]/.//tr[not(contains(.,'Invoice Number'))][" + (i+1) + "]/td");
+
+          row_object = {
+               invoice: r.snapshotItem(0),
+               status:  r.snapshotItem(1),
+               replace: r.snapshotItem(4)
+          }
+
+          var a = xpath_snapshot(".//a", row_object.replace);
+
+          /*
+                         if (debug) GM_log(i + ': Unavailable');
+                         p.style.display = 'none';
+                    } else {
+                         if (debug) GM_log(i + ': AVAILABLE');
+                         p.style.color = 'green';
+                    } */
+
+     }
+
+}
+
+
+// replace_links.snapshotItem(0).style.display = 'none';
+
+replace_links = xpath_snapshot(".//h3[contains(.,'Warranty Replacement Order History')]/..//table[1]/.//a[contains(.,'Replace')]");
 
 for (var i = 0; i < replace_links.snapshotLength; i++) {
      if (debug) GM_log('inspecting item: ' + i);
      var p = replace_links.snapshotItem(i);
      if (debug) GM_log('href: ' + p.getAttribute('href'));
 
+}
+
+// =-=-=-=-=- FUNCTIONS -=-=-=-=-= //
+
+function get_replacement_availability(url) {
+     var replacement_available = 0;
+
      var r = new XMLHttpRequest();
-     r.open('GET', p.getAttribute('href'));
+     r.open('GET', url);
      r.onreadystatechange = function () {
           if (r.readyState == 4) {
                if (r.status == 200) {
                     var q = r.getAllResponseHeaders();
-
                     
                     if (debug) GM_log(i + ': site returned ' + r.responseText.length + ' characters.');
-                    if (r.responseText.indexOf('alt="Unavailible" title="Replacement Availability"') != -1) {
-                         if (debug) GM_log(i + ': Unavailable');
-                         p.style.display = 'none';
-                    } else {
-                         if (debug) GM_log(i + ': AVAILABLE');
-                         p.style.color = 'green';
+                    if (r.responseText.indexOf('alt="Unavailible" title="Replacement Availability"') >= 0) {
+                         replacement_available = 1;
                     }
                }
           }
      };
      r.send(null);
+     
+     return replacement_available;
 }
 
-// =-=-=-=-=- FUNCTIONS -=-=-=-=-= //
 
 
 // =-=-=-=-=- Standard Functions -=-=-=-=-= //
 // The following functions are ones that I've created and use in pretty
 // much every script I write.
 
-function evaluate_xpath(xpath_query) {
-    if (debug >= 2) GM_log(xpath_query);
-    var nodes = document.evaluate(xpath_query, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+function xpath_snapshot(xpath_query, xml) {
+    if (!xml) xml = document;
+
+    if (debug >= 2) GM_log("xpath_snapshot: " + xpath_query);
+    var nodes = document.evaluate(xpath_query, xml, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
     if (debug >= 1) GM_log('number of nodes returned: ' + nodes.snapshotLength);
 
     return nodes;
+}
+
+function xpath_iterate(xpath_query, node) {
+    if (!node) node = document;
+
+    if (debug >= 2) GM_log("xpath_iterate: " + xpath_query);
+    var iterators = document.evaluate(xpath_query, node, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+    if (debug >= 1) GM_log('number of nodes returned: ' + nodes.snapshotLength);
+
+    return iterators;
+}
+
+function string_to_xml(s) {
+     var parser = new DOMParser();
+     var dom = parser.parseFromString(s, 'text/xml');
+
+     return dom;
+}
+
+function xml_to_string(xml) {
+     var serializer = new XMLSerializer();
+     var prettyString = XML(serializer.serializeToString(xml)).toXMLString();
+
+     return prettyString;
 }
