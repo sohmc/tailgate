2f4be0886d47258b3d2dd64de6a06e1849cb8858
Mike Soh <none@none>
2012-05-05 19:31:30 -0400

    Give me paste should work everywhere now that it detects paste events.

--- greasemonkey_scripts/branches/givemepaste-0.1/givemepaste.user.js
+++ greasemonkey_scripts/branches/givemepaste-0.1/givemepaste.user.js
@@ -3,7 +3,7 @@
 // @author      Michael Soh 
 // @namespace   8DDyj2fcDcsAEM29wn3pPzYS2XLwcBp1RQ3o8HAH_givemepaste
 // @description Will allow you to paste into input areas that have paste removed
-// @version     0.1
+// @version     0.2
 // @license     GPL 3.0 
 // @include     * 
 //  
@@ -12,5 +12,27 @@
 // @require     http://usocheckup.redirectme.net/UID.js
 
 $(document).ready(function() {
-     $('input[onpaste]').removeAttr('onpaste');
+     $('input').each(function() {
+          if ('onpaste' in this) {
+               this.removeAttribute('onpaste');
+
+               if ('onpaste' in this) {
+                    GM_log("Could not remove onpaste attribute from " + this.id);
+                    GM_log("onpaste still exists! Trying to set the function...");
+                    this.onpaste = function() { return true; }
+                    if ('onpaste' in this) GM_log("onpaste still exists! (3) No solution found.");
+               }
+          }
+          
+          if ('oncopy' in this) {
+               this.removeAttribute('oncopy');
+
+               if ('oncopy' in this) {
+                    GM_log("Could not remove oncopy attribute from " + this.id);
+                    GM_log("oncopy still exists! Trying to set the function...");
+                    this.oncopy = function() { return true; }
+                    if ('oncopy' in this) GM_log("oncopy still exists! (3) No solution found.");
+               }
+          }
+     });
 });
